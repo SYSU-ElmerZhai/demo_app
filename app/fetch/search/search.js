675cea55e3ce39e309b2c6c06a7002d47df62461
SYSU-ElmerZhai <zhaihd@mail2.sysu.edu.cn>
2017-07-29 09:41:31 +0800

    Search Page

---
+++ app/fetch/search/search.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Haodong on 2017/7/29.
+ */
+import { get } from '../get'
+
+export function getSearchData(page, cityName, category, keyword) {
+    const keywordStr = keyword ? '/' + keyword : ''
+    const result = get('/api/search/' + page + '/' + cityName + '/' + category + keywordStr)
+    return result
+}
