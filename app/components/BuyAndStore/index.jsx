850bfc1dc08d59b995e75ab17e9e914485f7c3ba
SYSU-ElmerZhai <zhaihd@mail2.sysu.edu.cn>
2017-08-04 09:29:04 +0800

    User Page

--- app/components/BuyAndStore/index.jsx
+++ app/components/BuyAndStore/index.jsx
@@ -12,12 +12,12 @@ class BuyAndStore extends React.Component {
         return (
             <div className="buy-store-container clear-fix">
                 <div className="item-container float-left">
-                    {
-                        // 是否已经收藏了
-                        this.props.isStore
-                            ? <button className="selected" onClick={this.storeClickHandle.bind(this)}>已收藏</button>
-                            : <button onClick={this.storeClickHandle.bind(this)}>收藏</button>
-                    }
+                {
+                    // 是否已经收藏了
+                    this.props.isStore
+                    ? <button className="selected" onClick={this.storeClickHandle.bind(this)}>已收藏</button>
+                    : <button onClick={this.storeClickHandle.bind(this)}>收藏</button>
+                }
                 </div>
                 <div className="item-container float-right">
                     <button onClick={this.buyClickHandle.bind(this)}>购买</button>
