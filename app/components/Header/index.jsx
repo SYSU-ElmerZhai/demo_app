541828c8ce3eb44616fbf6d88cbebf2d79530ff8
SYSU-ElmerZhai <zhaihd@mail2.sysu.edu.cn>
2017-08-06 23:54:19 +0800

    getready-combination

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-import { hashHistory } from 'react-router'
 
 import './style.less'
 
@@ -11,21 +10,10 @@ class Header extends React.Component {
     }
     render() {
         return (
-            <div id="common-header">
-                <span className="back-icon" onClick={this.clickHandle.bind(this)}>
-                    <i className="icon-chevron-left"></i>
-                </span>
-                <h1>{this.props.title}</h1>
+            <div>
+                <h1>Header</h1>
             </div>
         )
-    }
-    clickHandle() {
-        const backRouter = this.props.backRouter
-        if (backRouter) {
-            hashHistory.push(hashHistory)
-        } else {
-            window.history.back()
-        }
     }
 }
 
